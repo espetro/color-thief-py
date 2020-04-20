8a3bac04fb6fbbabe5d74e850c0f9663baaa6f47
Boyan Kolev <boyank@users.noreply.github.com>
2020-04-20 13:52:42 +0300

    Initialize ColorThief with PIL.Image, not filename
    ColorThief.__init__() will accept PIL.Image, not filename

--- colorthief.py
+++ colorthief.py
@@ -29,14 +29,12 @@ class cached_property(object):
 
 class ColorThief(object):
     """Color thief main class."""
-    def __init__(self, file):
+    def __init__(self, image):
         """Create one color thief for one image.
 
-        :param file: A filename (string) or a file object. The file object
-                     must implement `read()`, `seek()`, and `tell()` methods,
-                     and be opened in binary mode.
+        :param image: A PIL.Image.
         """
-        self.image = Image.open(file)
+        self.image = image
 
     def get_color(self, quality=10):
         """Get the dominant color.
